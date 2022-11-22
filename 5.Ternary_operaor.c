5c6b9a4c96fa783245bc28dcfd6688331804e83a
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 12:04:39 +0530

    Create 5.Ternary_operaor.c

---
+++ 5.Ternary_operaor.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main ()//main function
+{printf("RA2211042010042\n");
+ int num1 = 100;
+ int num2 = 200;
+ int maximum=(num1>num2)?num1:num2;
+ printf( "Max value is : %d\n", maximum);
+ return 0;
+}
+
