3a8b00d474e434a5519fb45e8aba1d042195dab4
SanyaMathur0411 <113873545+SanyaMathur0411@users.noreply.github.com>
2022-11-25 00:10:11 +0530

    Create Check_alphabets.c

---
+++ Check_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{   
+  printf("RA2211042010042\n");
+  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        printf("%c alphabet.", c);
+    else
+        printf("%c not alphabet.", c);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
