fee68f8f263c68982a0da7051c7c5e7d4c11e682
SanyaMathur0411 <113873545+SanyaMathur0411@users.noreply.github.com>
2022-11-24 23:28:08 +0530

    Create 10.Divisiblity_by_five.c

---
+++ 10.Divisiblity_by_five.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main() 
+{
+  int number;
+  printf("Enter the value of number: ");
+  scanf("%d",&number);
+  if (number%5 == 0) 
+  {
+    printf("%d is divisible by 5.", number);
+  }
+else
+{
+  printf(“ %d is not divisible by 5 .”,number);
+}
+} 
+
